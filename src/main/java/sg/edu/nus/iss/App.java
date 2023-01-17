c35e51e27f92db0504cc600cd82e6762b58a8ecc
Vincent N <VincentNSWE@gmail.com>
2023-01-17 10:42:18 +0800

    updating a bit

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -12,6 +12,11 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Thread thread1 = new Thread()
+    }
+
+
+
     }
 }
