e73facf0c2cb68756f2726e41b735fd6bcf1016b
Vincent N <VincentNSWE@gmail.com>
2023-01-17 10:48:44 +0800

    updating

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -13,9 +13,17 @@ public final class App {
      */
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread()
+        Thread thread1 = new Thread(new Runnable() {
+      
+        @Override
+        public void run() {
+            for (int i = 0; i< 20; i++) {
+                System.out.println(Thread.currentThread().getName() + "\tRunnable ...", + i);
+            }
+        }
     }
 
+    thread1.start();
 
 
     }
